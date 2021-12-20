d3b2741d2603a4ea524958498f2414e6b5515fee
Joaquin Perez Barroso <jperbar@Joaquins-MacBook-Pro-14.local>
2021-12-20 17:00:06 +0100

    more

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IndoorSDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "IndoorSDK",
+            targets: ["IndoorSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "IndoorSDK", url: "https://nutifront.s3.amazonaws.com/sdk_snapshots/", checksum: "8a172e53653c8811c35234a1cba8e0a712e774c8bc20a44aa32e1038f7cb8fb8")
+    ]
+)
