f41cdd033cda236f5734a7b7c0e73530d55ad609
Joaquin Perez Barroso <jperbar@me.com>
2021-12-23 14:23:56 +0100

    new version 1.2.3

--- Package.swift
+++ Package.swift
@@ -15,6 +15,6 @@ let package = Package(
 //        .package(url: "https://github.com/nutiteq/mobile-sdk-ios-metal-swift-package.git", from: "4.4.3"),
     ],
     targets: [
-        .binaryTarget(name: "IndoorSDK", url: "https://storage.googleapis.com/aena-xcframework/IndoorSDK-build44.xcframework.zip", checksum: "38627dbe28f560a7e2f0d839c6e3f9dc47b49452f80acfbfd5e0215d0a3533d9")
+        .binaryTarget(name: "IndoorSDK", url: "https://storage.googleapis.com/aena-xcframework/IndoorSDK.xcframework.zip", checksum: "38627dbe28f560a7e2f0d839c6e3f9dc47b49452f80acfbfd5e0215d0a3533d9")
     ]
 )
