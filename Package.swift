ec63cc4c08071191f4a0c6ff6e1601e18fe1d7a6
Joaquín Pérez Barroso <joaquinpbarroso@CartoMac-de-Joaquin.local>
2023-09-28 09:29:33 +0200

    Version 1.3.7

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "IndoorSDK", url: "https://storage.googleapis.com/aena-xcframework/IndoorSDK-1.3.6.zip", checksum: "b226f635c0cfd02043c8a639f8bced18eb2d3b3a922f67765eed28bb0c6f9f97")
+        .binaryTarget(name: "IndoorSDK", url: "https://storage.googleapis.com/aena-xcframework/IndoorSDK-1.3.7.zip", checksum: "791ca2242d40901f03fc490a8e4f56890ebf61422ecccea4b74618972e35be20")
     ]
 )
